b3daaa4cf50074cfb4a65b0f2142f739cdf086af
Apollon Oikonomopoulos <apoikos@debian.org>
2018-12-17 14:31:54 +0200

    Python: use `except … as` everywhere
    This is a small step to modernize the Python code. The `except … , x`
    syntax has been removed in Python 3 in favor of `except … as x`. The
    latter was introduced in Python 2.6, which is our minimum supported
    version, thus we can convert all uses of except to the "new" syntax.
    While at it, also remove all uses of `except … , _`.
    
    Signed-off-by: Apollon Oikonomopoulos <apoikos@debian.org>

--- lib/tools/cfgupgrade.py
+++ lib/tools/cfgupgrade.py
@@ -113,7 +113,7 @@ def OrFail(description=None):
       safety = copy.deepcopy(self.config_data)
       try:
         f(self)
-      except BaseException, e:
+      except BaseException as e:
         msg = "%s failed:\n%s" % (description or f.func_name, e)
         logging.exception(msg)
         self.config_data = safety
